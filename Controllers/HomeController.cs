2e34c7c3dc98a9aa741e74dee15dfb309faedcbc
urlockGaur <126732165+urlockGaur@users.noreply.github.com>
2024-03-13 22:26:51 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class HomeController : Controller
+    {
+         public ActionResult Index() => View();
+    }
+}
---
+++ Models/DataContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+  public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+  public DbSet<Category> Categories { get; set; }
+  public DbSet<Product> Products { get; set; }
+}
